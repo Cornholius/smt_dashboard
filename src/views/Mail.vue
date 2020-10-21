6a19d5b7cd48dc3785a28159d5d6038af7f1df6b
Ярослав <y.layshkin@gmail.com>
2020-10-21 13:49:20 +0300

    промежуточный

---
+++ src/views/Mail.vue
@@ -0,0 +1,15 @@
+<script>
+
+export default {
+  name: 'Mail',
+  watch: {
+    $route: {
+      immediate: true,
+      handler (to, from) {
+        document.title = to.meta.title || 'Почта'
+      }
+    }
+  }
+}
+
+</script>
---
+++ src/views/Request.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'requests',
+  components: {
+    HelloWorld
+  },
+  watch: {
+    $route: {
+      immediate: true,
+      handler (to, from) {
+        document.title = to.meta.title || 'Заявки'
+      }
+    }
+  }
+}
+
+</script>
